cea3f2a7b76dabc3f48307577f40287f6a7de985
claudsy <claudsy>
2019-09-04 16:29:47 -0400

    2.10 complete

--- Week 1/ViewController.swift
+++ Week 1/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
-    
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +20,35 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Awesome!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
         
-        if  messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
+        let messages = ["You Are Awesome!",
+        "You Are Great!",
+        "You Are Amazing!",
+        "You Are The Best!",
+        "You Are So Cool!",
+        "You Are The Pb To My J!",
+        "I can't wait to download your app!"]
+        
+        messageLabel.text = messages[index]
+        
+        if index == messages.count - 1 {
+            index = 0
         } else {
-            messageLabel.text = message1
+            index = index + 1
         }
+    
+        
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if  messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
    
     }
     
