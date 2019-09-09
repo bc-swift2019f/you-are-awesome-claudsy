4fc2d412968935c7efd751eb94ca22c8d538c748
claudsy <claudsy>
2019-09-09 02:08:44 -0400

    2.12 complete

--- Week 1/ViewController.swift
+++ Week 1/ViewController.swift
@@ -29,14 +29,17 @@ class ViewController: UIViewController {
         "You Are The Pb To My J!",
         "I can't wait to download your app!"]
         
-        messageLabel.text = messages[index]
+        messageLabel.text = messages.randomElement()!
         
-        if index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
-    
+        
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+//
         
 //        let message1 = "You Are Awesome!"
 //        let message2 = "You Are Great!"
