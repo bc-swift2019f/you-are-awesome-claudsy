a5b062b2964deaa0aa585ac076c401297fa91d5d
claudsy <claudsy>
2019-10-28 17:04:37 -0400

    fixed submission

--- Week 1/ViewController.swift
+++ Week 1/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
